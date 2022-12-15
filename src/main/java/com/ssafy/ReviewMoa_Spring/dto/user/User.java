1f5974b0953da968243b279d832f785d9b6a108b
lj05117 <lj05117@naver.com>
2022-12-15 23:21:47 +0900

    feat: add annotation

--- src/main/java/com/ssafy/ReviewMoa_Spring/dto/user/User.java
+++ src/main/java/com/ssafy/ReviewMoa_Spring/dto/user/User.java
@@ -1,11 +1,21 @@
 package com.ssafy.ReviewMoa_Spring.dto.user;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Getter
 @Setter
+@Entity
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String userId;
     private String userPwd;
     private String userName;
