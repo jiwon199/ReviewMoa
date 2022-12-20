59aca9788731668796a6ae8619597b79beeb0f51
jiwon <jiwonchoi99@naver.com>
2022-12-20 14:21:00 +0900

    조회수, 태그추가

--- src/main/java/com/ssafy/ReviewMoa_Spring/service/review/boardService.java
+++ src/main/java/com/ssafy/ReviewMoa_Spring/service/review/boardService.java
@@ -1,8 +1,8 @@
 package com.ssafy.ReviewMoa_Spring.service.review;
 import com.ssafy.ReviewMoa_Spring.dto.review.Board;
-import com.ssafy.ReviewMoa_Spring.dto.review.Content;
+import com.ssafy.ReviewMoa_Spring.dto.review.Tag;
 import com.ssafy.ReviewMoa_Spring.repository.review.boardRepository;
-import com.ssafy.ReviewMoa_Spring.repository.review.contentRepository;
+import com.ssafy.ReviewMoa_Spring.repository.review.tagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,30 +18,45 @@ public class boardService {
     private boardRepository boardRepo;
 
     @Autowired
-    private contentRepository contentRepo;
+    private tagRepository tagRepo;
 
     public void createPost(Board board) {
-//        for(Content content:board.getContentList()){
-//            content.setBoard(board);
-//        }
+        //f.k 설정
+        for(Tag tag:board.getTagList()){
+            tag.setBoard(board);
+        }
         //등록 시간 설정하고
         board.setRegistTime(LocalDate.now());
         //저장하기
         boardRepo.save(board);
     }
 
+    //목록 역순 정렬(최신순 정렬)
     public List<Board> getList() {
-        return boardRepo.findAll();
+        List <Board> list=boardRepo.findAll();
+        Collections.reverse(list);
+        return list;
     }
-
+    //게시글 하나 반환
     public Board getOne(Long postId) {
-        return boardRepo.getReferenceById(postId);
+        Board board=boardRepo.getReferenceById(postId);
+        return board;
     }
-
+    //조회수 갱신
+    public void updateHit(Long postId){
+        Optional<Board> entity = boardRepo.findById(postId);
+        entity.ifPresent(t ->{
+            int hit=t.getHit()+1;
+            t.setHit(hit);
+            boardRepo.save(t);
+        });
+    }
+    //삭제
     public void deleteOne(Long postId) {
         boardRepo.deleteById(postId);
     }
 
+    //수정
     public void updatePostInfo(Board board) {
         //바꿀 수 있는 것- genre, movieTitle,postTitle,thumbnail
         Optional<Board> entity = boardRepo.findById(board.getPostId());
@@ -62,23 +77,26 @@ public class boardService {
             if(board.getContent()!=null){
                 t.setContent(board.getContent());
             }
+            updatePostContent(board);
+
             // 이걸 실행하면 idx 때문에 update가 실행됨
             boardRepo.save(t);
         });
     }
-/*
+
+
+
     public void updatePostContent(Board board){
-        contentRepo.deleteByBoard(board); //다 지우고
+        tagRepo.deleteByBoard(board); //다 지우고
         //새로 fk 설정한 다음에
-        for(Content content:board.getContentList()){
-            content.setBoard(board);
+        for(Tag tag:board.getTagList()){
+            tag.setBoard(board);
         }
-        Collections.sort(board.getContentList(), Comparator.comparingInt(Content::getOrders));
-        contentRepo.saveAll(board.getContentList()); //새로 저장
+        //Collections.sort(board.getContentList(), Comparator.comparingInt(Tag::getOrders));
+        tagRepo.saveAll(board.getTagList()); //새로 저장
 
 
     }
 
- */
 
 }
