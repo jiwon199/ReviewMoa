5c4ea226833aea75b6f4dacc747ab3aa92907ae8
jiwon <jiwonchoi99@naver.com>
2022-12-22 21:57:10 +0900

    페이징 구현중

--- src/main/java/com/ssafy/ReviewMoa_Spring/repository/review/boardRepository.java
+++ src/main/java/com/ssafy/ReviewMoa_Spring/repository/review/boardRepository.java
@@ -2,7 +2,13 @@ package com.ssafy.ReviewMoa_Spring.repository.review;
 
 
 import com.ssafy.ReviewMoa_Spring.dto.review.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface boardRepository extends JpaRepository<Board, Long> {
+    public Page<Board> findAllByGenre(Pageable pageable, String genre);
+    public Page<Board> findByPostTitleContains(Pageable pageable, String postTitle);
 }
